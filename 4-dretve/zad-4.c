d3e33c44eab8bf08f5d389c49163472fdb0e5fd8
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-12-19 10:09:06 +0100

    LV4: slight modifications

--- 4-dretve/zad-4.c
+++ 4-dretve/zad-4.c
@@ -46,13 +46,17 @@ int main(int argc, char** argv) {
 		pthread_join(korisnici[i], NULL);
 	}
 	
+	int sum = 0;
 	for (i = 0; i < 10; i++) {
 		for (j = 0; j < 10; j++) {
 			printf("%d ", mjesta[i][j]);
+			sum += mjesta[i][j];
 		}
 		
-		puts("\n");
+		puts("");
 	}
+	
+	printf("sum = %d\n", sum);
 	
 	return 0;
 }
